50f9daf1eecc05ef84353b75603fbcdd3cad1f2b
hyungnam <zzzsh789@naver.com>
2020-03-31 23:13:17 +0900

    _error.js 추가, favicon 추가

--- front/components/ImagesZoom.js
+++ front/components/ImagesZoom.js
@@ -1,76 +1,9 @@
 import React, {useState} from "react";
 import PropTypes from "prop-types";
-import PostImages from "./PostImages";
 import Slick from 'react-slick';
-import {Icon} from "antd";
-import styled from 'styled-components';
+import {CloseBtn, Header, ImageWrapper, Indicator, Overlay, SlickWrapper} from './ImageZoomStyle';
 
-const Overlay = styled.div`
-    position: 'fixed';
-    z-index: 5000;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-`;
 
-// & 자기 자신을 가리키는 태그
-const Header = styled.header`
-    height: 44px;
-    background: white;
-    position: 0;
-    padding: 0;
-    text-align: center;
-    
-    & h1{
-        margin: 0;
-        font-size: 17px;
-        color: #333;
-        line-height: 44px;
-    }
-`;
-
-const H1 = styled.h1`
-
-`;
-
-const SlickWrapper = styled.div`
-    height: calc(100% - 44px);
-    background: #090909;
-`;
-
-const CloseBtn = styled(Icon)`
-    position: absolute;
-    right: 0;
-    top: 0;
-    padding: 15px;
-    line-height: 14px;
-    cursor: pointer;
-`
-
-const Indicator = styled.div`
-    text-align: center;
-    & > div {
-        width: 75px;
-        height: 30px;
-        line-height: 30px;
-        border-radius: 15px;
-        background: #313131;
-        display: inline-block;
-        text-align: center;
-        color: white;
-        font-size: 15px;
-    }
-`;
-
-const ImageWrapper = styled.div`
-    padding: 32px;
-    text-align:center;
-    & img{
-        margin: 0 auto;
-        max-height: 750px;
-    }
-`;
 
 //Slick 안에 반복문을 풀어놓으면 얘네가 알아서 이미지 슬라이더가 됨
 //React Slick 공홈에 cdn _app.js 에 넣어줘야함!
--- front/server.js
+++ front/server.js
@@ -6,6 +6,7 @@ const expressSession = require('express-session');
 const dotenv = require('dotenv');
 const dev = process.env.NODE_ENV !== 'develoment'; //개발모드
 const prod = process.env.NODE_ENV === 'production'; //배포모드
+const path = require('path');
 
 //express와 next 연결
 const app = next({ dev }); //true
@@ -19,6 +20,7 @@ app.prepare().then(() => {
    server.use(morgan('dev'));
    server.use(express.json());
    server.use(express.urlencoded({extended:true}));
+   server.use('/', express.static(path.join(__dirname, 'public')));
    server.use(cookieParser(process.env.COOKIE_SECRET)) //백엔드와 똑같이 설정 해줘야 함, 서버가 두갠데 쿠키 시크릿이 달라지면 서로의 쿠키를 해독 못함
    server.use(expressSession({
        resave: false,
